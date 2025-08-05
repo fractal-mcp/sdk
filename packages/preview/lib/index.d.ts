93e68357fc791326b3a6062d86e50a0013e2f6fa
Max Ockner <maxockner@gmail.com>
2025-08-05 19:43:25 -0400

    remove preview package + preview cli command

--- packages/preview/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function startServer(port?: number): Promise<import("vite").ViteDevServer>;
-declare const _default: {
-    startServer: typeof startServer;
-};
-export default _default;
